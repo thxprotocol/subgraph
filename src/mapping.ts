af51f2edf4501011aa28df2695b7ad80eee779df
Bram Rongen <bram@thx.network>
2022-03-09 12:27:56 +0100

    Initial commit

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,47 +1,19 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import {
-  AssetPoolFactory,
-  AssetPoolDeployed
-} from "../generated/AssetPoolFactory/AssetPoolFactory"
-import { ExampleEntity } from "../generated/schema"
+import { AssetPoolDeployed } from "../generated/AssetPoolFactory/AssetPoolFactory";
+import { AssetPool, Factory } from "../generated/schema";
 
 export function handleAssetPoolDeployed(event: AssetPoolDeployed): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let factory = Factory.load("1");
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  // if no factory yet, set up blank initial
+  if (factory == null) {
+    factory = new Factory("1");
+    factory.poolCount = 0;
   }
+  factory.poolCount++;
+  factory.save();
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.assetPool = event.params.assetPool
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // None
+  // // Entities only exist after they have been saved to the store;
+  const entity = new AssetPool(event.params.assetPool.toHex());
+  entity.address = event.params.assetPool;
+  entity.save();
 }
